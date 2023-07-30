47346f987a4534b419f4f310c5eb124ee559d127
Eln Far <farzaliyevelnur00@gmail.com>
2023-07-30 21:07:31 +0400

    data-tables

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,11 +3,18 @@
 import { Button } from "@/components/ui/button"
 import Heading from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
+import { Billboard } from "@prisma/client"
 
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
 
-export default function BillBoardClient() {
+interface Props {
+  data:BillboardColumn[]
+}
+
+export default function BillBoardClient({data}:Props) {
 
     const router = useRouter()
     const params = useParams()
@@ -16,13 +23,15 @@ export default function BillBoardClient() {
     <>
     <div className="flex items-center justify-between">
         <Heading
-        title="Boards (0)"
+        title={`Boards (${data.length})`}
         description="Manage Billboards for your store"
         />
 
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}><Plus className="mr-2 h-4 w-4"/>Add New</Button>
     </div>
     <Separator/>
+
+    <DataTable columns={columns} data={data}/>
     </>
   )
 }
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,30 @@
+import prismadb from "@/lib/prismadb";
 import BillBoardClient from "./components/client";
+import { BillboardColumn } from "./components/columns";
 
-export default function page() {
+export default async  function page({params}:{
+  params:{storeId:string}
+}) {
+
+  const billboards = await prismadb.billboard.findMany({
+      where: {
+        storeId:params.storeId
+      },
+      orderBy: {
+        createdAt:'desc'
+      }
+  })
+
+  const formatBill:BillboardColumn[] = billboards.map((item) => ({
+    id:item.id,
+    label:item.label,
+    createdAt:item.createdAt.toLocaleString()
+  })) 
+
   return (
     <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
-            <BillBoardClient/>
+            <BillBoardClient data={formatBill}/>
         </div>
     </div>
   )
